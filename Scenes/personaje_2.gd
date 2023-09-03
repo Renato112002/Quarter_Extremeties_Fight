053f620733f06126184cc245ba023a15e3d5f53d
Renato112002 <renato.valdes@ug.uchile.cl>
2023-09-03 18:16:12 -0300

    arreglos personajes

--- Scenes/personaje_2.gd
+++ Scenes/personaje_2.gd
@@ -1,75 +1,28 @@
 extends CharacterBody2D
 
-var speed = 300
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jump_speed = -500
-var lanzar_animation_playing = false
 
-@onready var _animated_sprite = $AnimatedSprite2D
 
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	var move_input = Input.get_action_strength("move_right2") - Input.get_action_strength("move_left2")
-
-	if move_input < 0:
-		if lanzar_animation_playing:
-			_animated_sprite.play("run-verde")
-		else:
-			_animated_sprite.play("run")
-		_animated_sprite.flip_h = true
-	elif move_input > 0:
-		if lanzar_animation_playing:
-			_animated_sprite.play("run-verde")
-		else:
-			_animated_sprite.play("run")
-		_animated_sprite.flip_h = false
-	else:
-		if is_on_floor():
-			if lanzar_animation_playing:
-				_animated_sprite.play("idle-verde")
-			else:
-				_animated_sprite.play("idle")
-		else:
-			_animated_sprite.stop() # Detiene la animación cuando está en el aire
-		_animated_sprite.flip_h = true
-	
-	# Manejar el lanzamiento
-	if Input.is_action_just_pressed("lanzar2"):
-		_animated_sprite.play("lanzar")
-		lanzar_animation_playing = true
-		
-		
-		
-func _physics_process(delta: float) -> void:
-	if !is_on_floor():
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	var move_input = Input.get_action_strength("move_right2") - Input.get_action_strength("move_left2")
-
 	# Handle Jump.
-	if Input.is_action_just_pressed("move_up2"):
-		if is_on_floor():
-			if lanzar_animation_playing:
-				_animated_sprite.play("jump-verde")
-			else:
-				_animated_sprite.play("jump")
-			velocity.y = jump_speed
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-	if move_input != 0:
-		velocity.x = move_input * speed
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = 0
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-# Agregar esta función para controlar el último frame de la animación de salto
-func _on_AnimatedSprite2D_animation_finished():
-	if _animated_sprite.animation == "jump":
-		_animated_sprite.frame = _animated_sprite.frame_count - 1
-	elif _animated_sprite.animation == "jump-verde":
-		_animated_sprite.frame = _animated_sprite.frame_count - 1
-		
-	elif _animated_sprite.animation == "lanzar":
-		_animated_sprite.frame = _animated_sprite.frame_count - 1
